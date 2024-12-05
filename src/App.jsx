edf7e7c9fe32de6acab32f5c53075278864fc057
amadousadialloh <amadousadialloh@gmail.com>
2024-12-05 13:52:58 -0600

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 // src/App.jsx
-import Home from './components/Home';
+import { useState } from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  const [navItem, setNavItem] = useState("");
+  const handleClick = (navItem) => {
+    setNavItem(navItem);
+    console.log(navItem);
+  };
+
+  const renderComponent = () => {
+    if (navItem === "home") {
+      return <Home />;
+    }
+  };
+
   return (
     <div>
-      
-      <Home />
-  
+      <Navbar handleClick={handleClick} />
+      <button onClick={() => handleClick("home")} type="submit">
+        Home
+      </button>
       <h1>Welcome to MyApp!</h1>
       <p>This is a simple React app with a navbar.</p>
+      <div>{renderComponent()}</div>
     </div>
-  )
+  );
 };
 
 export default App;
-
