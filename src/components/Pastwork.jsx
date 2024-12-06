03e2a15a5c2e08ce0b8ce8273d76cc3fc4dc9f49
angel9154 <angelgerenavazquez@outlook.com>
2024-12-06 09:58:53 -0400

    added some css

--- src/components/Pastwork.jsx
+++ src/components/Pastwork.jsx
@@ -1,8 +1,16 @@
 const PastWork = () => {
     return (
+        <>
       <p>
         Past Work
       </p>
+      <ul>
+  <li>Amadou Diallo - Software Developer | Projects: E-commerce Website, Mobile Banking App</li>
+  <li>Angel Gerena Vazquez - Graphic Designer | Projects: Logo Redesign for XYZ Corp, Website UI for ABC Studios</li>
+  <li>Michael Edwards II - Data Scientist | Projects: Predictive Analytics for Retail, Machine Learning Model for Stock Market</li>
+  <li>Nate Martin - Full Stack Developer | Projects: Social Media App, API for Payment Gateway</li>
+</ul>
+      </>
     );
   };
   
